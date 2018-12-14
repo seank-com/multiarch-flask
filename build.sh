b125bae1f2533dfa8d3bdfc933c6d7ca65a42640
Sean Kelly <home@seank.com>
2018-12-13 19:51:12 -0800

    updated

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
-docker build -t multiarch-uwsgi-nginx:x86_64 -f uwsgi-nginx/Dockerfile.x86_64 uwsgi-nginx/
-docker build -t multiarch-uwsgi-nginx-flask:x86_64 -f uwsgi-nginx-flask/Dockerfile.x86_64 uwsgi-nginx-flask/
 docker build -t multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo/
 
-docker build -t multiarch-uwsgi-nginx:aarch64 -f uwsgi-nginx/Dockerfile.aarch64 uwsgi-nginx/
-docker build -t multiarch-uwsgi-nginx-flask:aarch64 -f uwsgi-nginx-flask/Dockerfile.aarch64 uwsgi-nginx-flask/
 docker build -t multiarch-demo:aarch64 -f demo/Dockerfile.aarch64 demo/
 
 
-docker run -d --rm -p 80:80 multiarch-uwsgi-nginx-flask:x86_64
-docker exec  /bin/sh
+# docker run -d --rm -p 80:80 multiarch-demo:x86_64 /bin/sh
