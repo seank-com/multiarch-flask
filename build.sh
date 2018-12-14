a6260856614ec19faf78f4de3a0e7d2092c2b158
Sean Kelly <home@seank.com>
2018-12-14 15:37:03 -0800

    flask, nginx and uwsgi working

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
-docker build -t multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo/
+docker build -t seankelly/multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo/
 
-docker build -t multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
-
+docker build -t seankelly/multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
 
 # docker run -d --rm -p 80:80 multiarch-demo:x86_64 /bin/sh
