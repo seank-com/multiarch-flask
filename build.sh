6be8ae56c1ec4ed2a04c34e07ff36d857d0749bc
Sean Kelly <home@seank.com>
2018-12-13 20:03:46 -0800

    arms and legs

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo/
 
-docker build -t multiarch-demo:aarch64 -f demo/Dockerfile.aarch64 demo/
+docker build -t multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
 
 
 # docker run -d --rm -p 80:80 multiarch-demo:x86_64 /bin/sh
