829668a25fde931bcc1f40a4e6fe4febc8ba05d7
Sean Kelly <home@seank.com>
2018-12-19 17:29:24 -0800

    only tensorflow left

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-docker build -t seankelly/multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo/
+#docker build -t multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo
+#docker build -t multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
 
-docker build -t seankelly/multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
+docker build -t multiarch-bld -f bld/Dockerfile.multi bld/
 
 # docker run -d --rm -p 80:80 multiarch-demo:x86_64 /bin/sh
