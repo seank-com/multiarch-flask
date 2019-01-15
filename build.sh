c9b196096feaae09d2dbf8f8babbd081b1d470bd
Sean Kelly <home@seank.com>
2019-01-15 13:50:26 -0800

    updates

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #docker build -t multiarch-demo:x86_64 -f demo/Dockerfile.x86_64 demo
 #docker build -t multiarch-demo:arm64 -f demo/Dockerfile.arm64 demo/
 
+#docker run --rm --privileged multiarch/qemu-user-static:register --reset
 docker build -t multiarch-bld -f bld/Dockerfile.multi bld/
 
 # docker run -d --rm -p 80:80 multiarch-demo:x86_64 /bin/sh
