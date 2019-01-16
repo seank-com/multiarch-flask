fb84f39937f0849fbfb5ac88ddd67b430670d1b4
Sean Kelly <home@seank.com>
2019-01-16 11:09:26 -0800

    old style multi-stage

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 # Build Bazel (native) - [does not build emulated](https://github.com/bazelbuild/bazel/issues/7135)
 
-#docker build -t multiarch-bazel -f bld/Dockerfile.bld-bazel bld/
-#docker tag multiarch-bazel seankelly/multiarch-bazel:arm64-latest
+docker build -t multiarch-bazel -f bld/Dockerfile.bld-bazel bld/
+docker tag multiarch-bazel seankelly/multiarch-bazel:arm64-latest
 #docker push seankelly/multiarch-bazel:arm64-latest
 
 # Build TensorFlow (emulated)
 
 #docker run --rm --privileged multiarch/qemu-user-static:register --reset
+docker container create --name extract multiarch-bazel
+docker container cp extract:/root/bazel-0.21.0/output/bazel ./bazel
+docker container rm -f extract
+
 docker build -t multiarch-bld -f bld/Dockerfile.bld-tf bld/
 
 # Build Flask (native and emulated)
