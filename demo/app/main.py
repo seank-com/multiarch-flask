6e0cb37d1d255cf1c7962eaced1693898484d4e0
Sean Kelly <home@seank.com>
2019-01-21 17:11:19 -0800

    fewer bugs

--- demo/app/main.py
+++ demo/app/main.py
@@ -3,23 +3,21 @@ app = Flask(__name__)
 
 import tensorflow as tf
 with tf.device('/gpu:0'):
-        a = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[2,3],name='a')
-        b = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[3,2],name='b')
-        c = tf.matmul(a,b)
-
+  a = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[2,3],name='a')
+  b = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[3,2],name='b')
+  c = tf.matmul(a,b)
 
 @app.route("/")
 def hello():
-     out = "Hello World from Flask with TensorFlow " + \
-           "in a uWSGI Nginx Docker container " + \
-           "with Python 3.5\n" + \
+  out = "Hello World from Flask with TensorFlow " + \
+        "in a uWSGI Nginx Docker container " + \
+        "with Python 3.5\n"
 
-     with tf.Session() as sess:
-          out = out + sess.run(c)
+  with tf.Session() as sess:
+    out = out + sess.run(c)
 
-    return out 
-
+  return out 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+  app.run(host='0.0.0.0', debug=True, port=80)
     
