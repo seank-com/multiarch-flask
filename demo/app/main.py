9c249ed77a8961ef15c14cc746994ca8c53136d5
Sean Kelly <home@seank.com>
2019-01-21 16:35:44 -0800

    cleanerish

--- demo/app/main.py
+++ demo/app/main.py
@@ -1,11 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
 
+import tensorflow as tf
+with tf.device('/gpu:0'):
+        a = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[2,3],name='a')
+        b = tf.constant([1.0,2.0,3.0,4.0,5.0,6.0], shape=[3,2],name='b')
+        c = tf.matmul(a,b)
+
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask with TensorFlow in a uWSGI Nginx Docker container with \
-     Python 3.6 (default)"
+     out = "Hello World from Flask with TensorFlow " + \
+           "in a uWSGI Nginx Docker container " + \
+           "with Python 3.5\n" + \
+
+     with tf.Session() as sess:
+          out = out + sess.run(c)
+
+    return out 
 
 
 if __name__ == "__main__":
